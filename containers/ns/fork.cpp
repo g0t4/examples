43a393a812346f2023625d1eafb4d940d686ddaa
Wes Higbee <wes.mcclure@gmail.com>
2024-05-01 16:35:55 -0500

    mod

--- containers/ns/fork.cpp
+++ containers/ns/fork.cpp
@@ -1,14 +1,17 @@
 #include <iostream>
 #include <unistd.h>   // for fork()
 #include <sys/wait.h> // for wait()
+#include <sched.h>    // for unshare()
 
 int main()
 {
     printf("\n");
 
-    printf("PID: %d\n", getpid());
+    printf("getpid(): %d\n", getpid());
 
+    unshare(CLONE_NEWPID); // ignore failures to keep code readabl-er
     pid_t fork_pid = fork();
+    // move unshare here (after fork) to show difference in PIDs for child
 
     if (fork_pid > 0)
     {
