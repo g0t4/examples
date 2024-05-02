9b05afcfadc58794b50407cbb402729ebfc783eb
Wes Higbee <wes.mcclure@gmail.com>
2024-05-01 19:48:04 -0500

    first video

--- containers/ns/fork/fork.cpp
+++ containers/ns/fork/fork.cpp
@@ -12,6 +12,22 @@ int main()
     printf("\n");
 
     cout << "getpid() = " << getpid() << endl;
+    unshare(CLONE_NEWPID);
+    cout << "getpid() = " << getpid() << endl;
+
+    cout << "before fork\n";
+    int fork_pid = fork();
+    cout << "after fork\n";
+    if (fork_pid == 0)
+    {
+        // child
+        cout << "[Child] " << getpid() << ", fork_pid=" << fork_pid << endl;
+    }
+    else if (fork_pid > 0)
+    {
+        // parent
+        cout << "[Parent] " << getpid() << ", fork_pid=" << fork_pid << endl;
+    }
 
     printf("\n\n");
     return 0;
