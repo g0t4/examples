c470911a35c4f05b5e22df060946809ed7b2a0c9
Wes Higbee <wes.mcclure@gmail.com>
2024-05-03 15:59:54 -0500

    notes

--- containers/ns/fork/unshare.sh
+++ containers/ns/fork/unshare.sh
@@ -1,9 +1,17 @@
 
 
+# *** unshare command reference:
+
 # unshare(CLONE_NEWPID) equivalent to:
 unshare --pid/-p
+#    parsed: https://github.com/util-linux/util-linux/blob/e3192bfd1dd129c70f5416e1464135d8cd22c956/sys-utils/unshare.c#L892-L896
+#    unshare called: https://github.com/util-linux/util-linux/blob/e3192bfd1dd129c70f5416e1464135d8cd22c956/sys-utils/unshare.c#L1025-L1026
+
 # fork() equivalent to:
 unshare --fork/-f
+#    parsed: https://github.com/util-linux/util-linux/blob/e3192bfd1dd129c70f5416e1464135d8cd22c956/sys-utils/unshare.c#L869-L871
+#    fork called: https://github.com/util-linux/util-linux/blob/e3192bfd1dd129c70f5416e1464135d8cd22c956/sys-utils/unshare.c#L1054-L1069
+
 # combined:
 unshare --fork --pid
 unshare -f -p
