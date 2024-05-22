182a6f9bb1b0e3d5acdd5af22f1c0c6778b258ea
Wes Higbee <wes.mcclure@gmail.com>
2024-05-22 18:20:49 -0500

    rewrite

--- semantic/Program.cs
+++ semantic/Program.cs
@@ -31,9 +31,10 @@ using Microsoft.SemanticKernel;
 using Microsoft.SemanticKernel.ChatCompletion;
 using Microsoft.SemanticKernel.Connectors.OpenAI;
 
+
 #pragma warning disable SKEXP0010 // Type is for evaluation purposes only and is subject to change or removal in future updates. Suppress this diagnostic to proceed.
-// FYI model is hard coded on lm studio side, so modelId is irrelevant here
-var openai_ccs = new OpenAIChatCompletionService(modelId: "foo", apiKey: "lm-studio:", endpoint: new Uri("http://localhost:1234/v1/chat/completions"));
+var openai_ccs = Kernel.CreateBuilder()
+  .AddOpenAIChatCompletion(modelId: "foo", apiKey: "lm-studio:", endpoint: new Uri("http://localhost:1234/v1/chat/completions");
 #pragma warning restore SKEXP0010 // Type is for evaluation purposes only and is subject to change or removal in future updates. Suppress this diagnostic to proceed.
 
 // semantic kernel builder
