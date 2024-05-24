e1431ed46630be323565df9c8e795f92d5a6efd4
Wes Higbee <wes.mcclure@gmail.com>
2024-05-24 16:50:06 -0500

    refactor

--- lms/autogen/guess-number.py
+++ lms/autogen/guess-number.py
@@ -1,5 +1,7 @@
 from autogen import ConversableAgent
 
+number_to_guess = "254"
+
 llm_config = {
     "config_list": [{
         "model": "gpt-4o",
@@ -10,10 +12,10 @@ llm_config = {
 agent_with_number = ConversableAgent(
     "agent_with_number",
     system_message="You are playing a game of guess-my-number. You have the "
-    "number 254 in your mind, and I will try to guess it. "
+    f"number {number_to_guess} in your mind, and I will try to guess it. "
     "If I guess too high, say 'too high', if I guess too low, say 'too low'. ",
     llm_config=llm_config,
-    is_termination_msg=lambda msg: "254" in msg["content"],  # terminate if the number is guessed by the other agent
+    is_termination_msg=lambda msg: number_to_guess in msg["content"],  # terminate if the number is guessed by the other agent
     human_input_mode="NEVER",  # never ask for human input
 )
 
