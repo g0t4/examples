d3a1f78d1a20252e626610530a48546fa3860ba3
Wes Higbee <wes.mcclure@gmail.com>
2024-05-23 19:00:26 -0500

    woa I got code llama to do it... and it was hilarious

--- lms/autogen/demo.py
+++ lms/autogen/demo.py
@@ -11,4 +11,4 @@ config_list = [
 assistant = AssistantAgent("assistant", llm_config={"config_list": config_list})
 
 user_proxy = UserProxyAgent("user_proxy", code_execution_config={"work_dir": "coding", "use_docker": False})
-user_proxy.initiate_chat(assistant, message="Plot a chart of NVDA and TESLA stock price change YTD.")
+user_proxy.initiate_chat(assistant, message="make a plot of a penis graph.")
