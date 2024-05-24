37b262c8f95fd73217b8e1f7f03ece65bb235826
Wes Higbee <wes.mcclure@gmail.com>
2024-05-23 19:17:28 -0500

    use ollama

--- lms/autogen/guess-number.py
+++ lms/autogen/guess-number.py
@@ -2,12 +2,22 @@ import os
 
 from autogen import ConversableAgent
 
+config_list = [
+  {
+    "model": "codellama",
+    "base_url": "http://localhost:11434/v1",
+    "api_key": "ollama",
+  }
+]
+llm_config={"config_list": config_list}
+# llm_config={"config_list": [{"model": "gpt-4", "api_key": os.environ["OPENAI_API_KEY"]}]}
+
 agent_with_number = ConversableAgent(
     "agent_with_number",
     system_message="You are playing a game of guess-my-number. You have the "
     "number 53 in your mind, and I will try to guess it. "
     "If I guess too high, say 'too high', if I guess too low, say 'too low'. ",
-    llm_config={"config_list": [{"model": "gpt-4", "api_key": os.environ["OPENAI_API_KEY"]}]},
+    llm_config=llm_config,
     is_termination_msg=lambda msg: "53" in msg["content"],  # terminate if the number is guessed by the other agent
     human_input_mode="NEVER",  # never ask for human input
 )
@@ -17,7 +27,7 @@ agent_guess_number = ConversableAgent(
     system_message="I have a number in my mind, and you will try to guess it. "
     "If I say 'too high', you should guess a lower number. If I say 'too low', "
     "you should guess a higher number. ",
-    llm_config={"config_list": [{"model": "gpt-4", "api_key": os.environ["OPENAI_API_KEY"]}]},
+    llm_config=llm_config,
     human_input_mode="NEVER",
 )
 
