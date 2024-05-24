7598bb019dbda2ac49cd23bbb11a19bdae8999d6
Wes Higbee <wes.mcclure@gmail.com>
2024-05-24 18:22:03 -0500

    find a file via python and shell code

--- lms/autogen/find-the-file.py
+++ lms/autogen/find-the-file.py
@@ -1,22 +1,18 @@
-import os
-
 from autogen import UserProxyAgent, AssistantAgent
 
-config_list = [
-  {
-    "model": "codellama",
-    "base_url": "http://localhost:11434/v1",
-    "api_key": "ollama",
-  }
-]
-llm_config={"config_list": config_list}
-# llm_config={"config_list": [{"model": "gpt-4", "api_key": os.environ["OPENAI_API_KEY"]}]}
+# openai gets it right wihthin 1 or 2 tries, awesome!
+llm_config = {
+    "config_list": [{
+        "model": "gpt-4o",
+        "base_url": "https://api.openai.com/v1",
+    }]
+}
 
 assistant = AssistantAgent(
     "assistant",
-    system_message="You are tasked with finding a file that I lost, in the current working directory. I only recall that it has the word 'foo' inside of it. You must devise code to find the file, writtin in python",
+    system_message="You are tasked with finding a file that I lost, in the current working directory. I only recall that it has the word 'foo' inside of it. Find it and show me the contents of it. You must devise code to find the file, writtin in python",
     llm_config=llm_config,
-    is_termination_msg=lambda msg: "test.txt" in msg["content"]
+    is_termination_msg=lambda msg: "foo the bar" in msg["content"]
 )
 
 human = UserProxyAgent(
