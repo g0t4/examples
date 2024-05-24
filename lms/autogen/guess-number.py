f1a8a7e3c6f48aa9f06389a73596f43cfd53d6d4
Wes Higbee <wes.mcclure@gmail.com>
2024-05-24 16:48:13 -0500

    try openai wow

--- lms/autogen/guess-number.py
+++ lms/autogen/guess-number.py
@@ -1,24 +1,19 @@
-import os
-
 from autogen import ConversableAgent
 
-config_list = [
-  {
-    "model": "codellama",
-    "base_url": "http://localhost:11434/v1",
-    "api_key": "ollama",
-  }
-]
-llm_config={"config_list": config_list}
-# llm_config={"config_list": [{"model": "gpt-4", "api_key": os.environ["OPENAI_API_KEY"]}]}
+llm_config = {
+    "config_list": [{
+        "model": "gpt-4o",
+        "base_url": "https://api.openai.com/v1",
+    }]
+}
 
 agent_with_number = ConversableAgent(
     "agent_with_number",
     system_message="You are playing a game of guess-my-number. You have the "
-    "number 53 in your mind, and I will try to guess it. "
+    "number 254 in your mind, and I will try to guess it. "
     "If I guess too high, say 'too high', if I guess too low, say 'too low'. ",
     llm_config=llm_config,
-    is_termination_msg=lambda msg: "53" in msg["content"],  # terminate if the number is guessed by the other agent
+    is_termination_msg=lambda msg: "254" in msg["content"],  # terminate if the number is guessed by the other agent
     human_input_mode="NEVER",  # never ask for human input
 )
 
@@ -33,5 +28,5 @@ agent_guess_number = ConversableAgent(
 
 result = agent_with_number.initiate_chat(
     agent_guess_number,
-    message="I have a number between 1 and 100. Guess it!",
+    message="I have a number between 1 and 1000. Guess it!",
 )
