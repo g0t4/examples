26862edaa77b442dfda78af56fc5ee205ca212d3
Wes Higbee <wes.mcclure@gmail.com>
2024-09-01 02:26:22 -0500

    notes

---
+++ vms/linux-next-kernel/test-dri-drm.sh
@@ -0,0 +1,8 @@
+
+# not sure I needed these all but I got kmscube to work after these:
+sudo apt update
+sudo apt install -y kmscube
+
+sudo apt install -y mesa-vulkan-drivers mesa-utils
+sudo apt install -y mesa-va-drivers mesa-vdpau-drivers
+
