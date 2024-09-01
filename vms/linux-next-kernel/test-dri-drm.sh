82aa6469cdadf355d07da28a47804924901de158
Wes Higbee <wes.mcclure@gmail.com>
2024-09-01 03:02:58 -0500

    left off here ... w/o spice me thinks I cannot get DRI/DRM to work for a VM so lets stop for now... I need qemu with spice to MAYBE have DRI rendering so that when the panic happens the screen still shows

--- vms/linux-next-kernel/test-dri-drm.sh
+++ vms/linux-next-kernel/test-dri-drm.sh
@@ -1,8 +1,17 @@
 
-# not sure I needed these all but I got kmscube to work after these:
 sudo apt update
 sudo apt install -y kmscube
-
+# kmscube failed, so I added these per chatgpt suggestion:
 sudo apt install -y mesa-vulkan-drivers mesa-utils
 sudo apt install -y mesa-va-drivers mesa-vdpau-drivers
 
+kmscube # runs over SSH now... and renders frames (log output), no cube here b/c...
+# VNC in to see the CUBE!!!! WORKS
+
+ls /dev/dri # has dri devices too!
+
+
+# *** spice grpahics fails:
+virsh shutdown u2410
+virsh define u2410 # spice graphics are not supported with this QEMU
+# seems like my qemu build doesn't support spice graphics?!
