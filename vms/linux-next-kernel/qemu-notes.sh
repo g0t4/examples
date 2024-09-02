604865ac7701fd50bfc80aaee86b14380474726d
Wes Higbee <wes.mcclure@gmail.com>
2024-09-02 01:58:05 -0500

    notes

--- vms/linux-next-kernel/qemu-notes.sh
+++ vms/linux-next-kernel/qemu-notes.sh
@@ -5,4 +5,5 @@ sudo sed -i 's/^GRUB_DEFAULT=.*/GRUB_DEFAULT="1>2"/' /etc/default/grub
 # 1 => is 2nd item which is "Advanced options for Ubuntu"
 # 2 => is 3rd item which is "Ubuntu... qrcodewes..."
 sudo update-grub
-sudo reboot # watch on VNC => selects advanced menu entry and waits... at timeout it autoboots to the one I want (I wonder if I hit return if the sub menu shows with 3rd item selected then)
+sudo reboot # watch on VNC => selects advanced menu entry and waits... at timeout it autoboots to the one I wantj
+# FYI if default is a multi select 1>2 then if you hit enter while waiting it will select the submenu item too... so enter enter skips waiting
