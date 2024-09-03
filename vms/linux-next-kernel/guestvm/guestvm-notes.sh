ea20050a36fc4da0666c11ad494489d914dbccbd
Wes Higbee <wes.mcclure@gmail.com>
2024-09-03 08:51:24 -0500

    notes

--- vms/linux-next-kernel/guestvm/guestvm-notes.sh
+++ vms/linux-next-kernel/guestvm/guestvm-notes.sh
@@ -7,7 +7,7 @@ sudo usermod -aG libvirt,kvm $USER
 # logout/kill ssh sessions
 
 
-# aside - my dotfiles for fish..
+# aside - my dotfiles for fish (on build13)..
 git clone https://github.com/g0t4/dotfiles.git ~/repos/github/g0t4/dotfiles
 ln --force -s ~/repos/github/g0t4/dotfiles/fish/config/config.fish ~/.config/fish/config.fish  # link to my fish config, logout/in to source
 source ~/repos/github/g0t4/dotfiles/fish/install/install.fish # installs fisher + z
@@ -158,7 +158,27 @@ sudo systemctl start ssh.service # was stoppped and disabled
 ssh wes@192.168.122.180 # from build13 WORKS! good enough for me, screw the published port to the host
 
 
+## OMG blacklist qxl driver worked... but I dont think the resolution is large?
+#   GRUB_CMDLINE_LINUX_DEFAULT="video=simpledrm modprobe.blacklist=qxl"
+#   drm_info shows simpledrm (can at least test panic screen though!)
+sudo fbset -s # shows 1280x800! bigger by a smidge ... might enough w/o needing spice + simpledrm (if that even does anything different)
 
-
-
+# scp'ing built kernel from host to guestvm
+# WIP => installing build tools to be able to run make install  in VM which yikez means I need clang/rust/etc... NBD just do it when time permits
+#    apt installs done for make
+#    was in middle of notes-build13-qrcode's steps for clang and that is when VM froze FUUU... I wonder if my custom kernel is a bit brittle :)  and I should take host back to stable ubuntu 6.8
+#         freezing happened one other time and `virsh destroy guestvm` DOES NOTHING (hangs then fails) => had to reboot last time
 #
+#
+#   FALLBACK is to recompile entire kernel in the guestvm from scratch but I would like to avoid that
+#     though I could do it overnight?!
+#
+#
+
+GRUB_DEFAULT="1>4" # 5th entry, 6.8 original kernel should be more stable
+
+# add entry to /etc/hosts
+#    192.168.122.180 guestvm
+#
+# got guestvm configured for fish shell and other tools I like
+sudo hostnamectl set-hostname guestvm
