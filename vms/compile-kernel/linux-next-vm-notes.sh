7c5266905e9169838d1f7323b38e61acdf2e5284
Wes Higbee <wes.mcclure@gmail.com>
2024-09-04 23:02:43 -0500

    test recompile DRM_PANIC

--- vms/compile-kernel/linux-next-vm-notes.sh
+++ vms/compile-kernel/linux-next-vm-notes.sh
@@ -2,6 +2,10 @@
 vagrant init wesdemos/ubuntu2410-arm
 vagrant up
 
+# setup direct ssh access to VM
+vagrant ssh-config --host compile-kernel  > ~/.ssh/config.d/vagrant-compile-kernel
+trash ~/.ssh/config.d/vagrant-compile-kernel
+
 
 # *** generate config
 # route A
@@ -22,19 +26,19 @@ scripts/config --set-str CONFIG_SYSTEM_REVOCATION_KEYS ""
 
 
 # *** compile
-time make -j$(nproc) # 342.74 secs
+time make -j$(nproc) # 342.74 secs, 331s round2
 
 # *** TODO repeat compile tests with LLVM/clang
 
 
 # make modules_install
-time sudo make modules_install # 1 second!
+time sudo make modules_install # 1 second, 1s round2
 
 time sudo make install # FAILS:
 # b/c dkms + parallels-tools
 sudo dkms status # shows parallel-tools
 sudo dkms remove -m parallels-tools -v 19.4.0.54962 --all
-time sudo make install # 2.51 seconds!
+time sudo make install # 2.51 seconds, 3s round2
 
 # FYI remove old kernel installs:
 sudo rm /boot/*6.11.0*
@@ -53,4 +57,36 @@ sudo sed -i 's/^GRUB_TIMEOUT=.*/GRUB_TIMEOUT=5/' /etc/default/grub
     sudo update-grub
 # show grub menu:
 sudo sed -i 's/^GRUB_TIMEOUT_STYLE=.*/GRUB_TIMEOUT_STYLE=menu/' /etc/default/grub
+    sudo update-grub
+
+
+#
+# cp backup config before mrproper:
+make clean
+make mrproper
+# cp .config back same as before
+# crap llvm isn't supported on 24.10 currently... so come back to this later
+
+# *** (round2) lets retime gcc after clean/mrproper
+
+# *** (round3) test speed to recompile w/ panic screen (after full compile worked)
+
+grep DRM_PANIC .config
+scripts/config --enable DRM_PANIC
+scripts/config --enable DRM_PANIC_DEBUG
+make olddefconfig # set defaults on other DRM_PANIC options:
+# CONFIG_DRM_PANIC=y                                                                           # CONFIG_DRM_PANIC is not set
+# CONFIG_DRM_PANIC_FOREGROUND_COLOR=0xffffff
+# CONFIG_DRM_PANIC_BACKGROUND_COLOR=0x000000
+# CONFIG_DRM_PANIC_DEBUG=y
+# CONFIG_DRM_PANIC_SCREEN="user"
+cp .config ../configs/02-gcc-w-drm-panic.config
+time make -j$(nproc) # round3: 36s
+sudo rm /boot/*6.11.0*
+time sudo make modules_install # round3: 1s
+time sudo make install # round3: 3s
+
+# todo blacklist gpu drivers (uses simpledrm before virtio_gpu)
+# GRUB_CMDLINE_LINUX="video=simpledrm "
+sudo sed -i 's/^GRUB_CMDLINE_LINUX=.*/GRUB_CMDLINE_LINUX="modprobe.blacklist=virtio_gpu"/' /etc/default/grub
     sudo update-grub
