07c81544b4ce45a3ccbe081a4e3888f0fc0abfea
Wes Higbee <wes.mcclure@gmail.com>
2024-09-05 00:38:32 -0500

    notes

--- vms/compile-kernel/wes-recompile-install.sh
+++ vms/compile-kernel/wes-recompile-install.sh
@@ -1,14 +1,15 @@
 time make -j$(nproc)
 
 echo
-echo 
+echo
 
 time sudo make modules_install
 
 echo
-echo 
+echo
 
+# remove old kernels b/c limited space on /boot partition and also I don't wanna see all of them in grub
 sudo rm /boot/*6.11.0*
 echo
-echo 
+echo
 time sudo make install
