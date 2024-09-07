f46da4f352681419937cad62f5cb1a9b6cc1c6ab
Wes Higbee <wes.mcclure@gmail.com>
2024-09-07 14:11:36 -0500

    notes

--- vms/compile-rust/filemod/notes.sh
+++ vms/compile-rust/filemod/notes.sh
@@ -21,3 +21,17 @@ modprobe -c
 
 make LLVM=-18 # compile modules w/ same compiler and version used to compile kernel (in my rust env I am using LLVM tooling v18)
 modinfo filemod.ko # shows metadata from module source file
+modinfo hid # find module and show its info, hid is in:
+sudo ls  /lib/modules/$(uname -r)/ # IIUC from make modules_install (kernel modules)
+# FYI modinfo shows:
+# intree:
+# depends:
+# filename:
+
+# load hello world module (filemod)
+insmod filemod.ko
+lsmod | grep filemod
+sudo dmesg | tail -n 1
+rmmod filemod
+sudo dmesg | tail -n 1
+
