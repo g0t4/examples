14eddc90089782cff7c99e6e46b3a36b6d5bd115
Wes Higbee <wes.mcclure@gmail.com>
2024-09-07 16:34:37 -0500

    work on virtual file

--- vms/compile-rust/filemod/dht22-notes.sh
+++ vms/compile-rust/filemod/dht22-notes.sh
@@ -6,3 +6,12 @@ ls /dev/dht22
 
 sudo cat /dev/dht22 # hangs indefinitely currently
 echo "foo" | sudo tee /dev/dht22 # works (NOOP currently)
+
+sudo rmmod dht22 # unload works...
+ls /dev/dht22 # gone! good
+
+# ok its working:
+sudo rmmod dht22; sudo insmod dht22.ko
+sudo cat /dev/dht22 # shows initial message
+echo "foo" | sudo tee /dev/dht22
+sudo cat /dev/dht22 # shows "foo"... TODO erase rest of msg array to cover data beyond what is written? or no?
--- vms/compile-rust/filemod/dht22.c
+++ vms/compile-rust/filemod/dht22.c
@@ -10,29 +10,38 @@ static int major;
 static struct class *my_class;
 static struct cdev my_cdev;
 
-static int dht22_open(struct inode *inode, struct file *file) {
+static int dht22_open(struct inode *inode, struct file *file)
+{
     pr_info("dht22_open opened\n");
     return 0;
 }
 
-static int dht22_release(struct inode *inode, struct file *file) {
+static int dht22_release(struct inode *inode, struct file *file)
+{
     pr_info("dht22_release closed\n");
     return 0;
 }
 
+// store string of content in memory
+static char msg[100] = "Hello from dht22\n";
 
-static ssize_t cached_value = 10;
-
-static ssize_t dht22_read(struct file *file, char __user *buf, size_t len, loff_t *offset) {
-    pr_info("dht22_read called\n");
-    return cached_value;
+static ssize_t dht22_read(struct file *file, char __user *buf, size_t len, loff_t *offset)
+{
+    pr_info("dht22_read called with len: %d, offset: %lld\n", (int)len, *offset);
+    // prn use offset?
+    return simple_read_from_buffer(buf, len, offset, msg, strlen(msg));
 }
 
-static ssize_t dht22_write(struct file *file, const char __user *buf, size_t len, loff_t *offset) {
+static ssize_t dht22_write(struct file *file, const char __user *buf, size_t len, loff_t *offset)
+{
     pr_info("dht22_write called\n");
-    return len;
+    // erase msg (probably shoudn't do this and instead let it overwrite subset of msg (offset/len) but meh... just erase it all for now on each write)
+    memset(msg, 0, sizeof(msg)); // null terminate entire msg buffer so the new message is it
+    // PRN use offset
+    return simple_write_to_buffer(msg, sizeof(msg), offset, buf, len);
 }
 
+// FYI defined: include/linux/fs.h
 static struct file_operations fops = {
     .owner = THIS_MODULE,
     .open = dht22_open,
