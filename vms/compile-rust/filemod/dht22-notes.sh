a29ef7f661d4befc03b2a3c859405b47351f4b72
Wes Higbee <wes.mcclure@gmail.com>
2024-09-07 21:18:35 -0500

    dht22 all done on pi6 now

--- vms/compile-rust/filemod/dht22-notes.sh
+++ vms/compile-rust/filemod/dht22-notes.sh
@@ -1,6 +1,23 @@
+# prepare pi6 for testing dht22 sensor
 
-# wait do I need to create the device node still... chatgpt said to do this:
+# compile fresh new kernel, minimal config...
+cp /boot/config-$(uname -r) ~/linux-next/.config
+yes "" | make localmodconfig # strip down + defaults
 
-# I want the driver to do this?
-sudo mknod /dev/dht22 c <major_number> 0
-# PRN use rwdev to redo /dev/dht22 to be full service within the driver?
+mkdir ~/configs
+cp .config ~/configs/01-localmodconfig
+
+make menuconfig
+# search / RASPBERRY
+# GPIO_RASPBERRYPI_EXP =y .. probably what I want then... #  TODO ensure GPIO modules/drivers enabled?
+# exit dont save
+
+# compile new kernel
+time make -j$(nproc) # using gcc this time
+time sudo make modules_install
+time sudo make install
+
+# issue with initrd/ramfs... just realized I don't need a custom kernel on my pi... just use the stable builtin ones.. I don't need 6.11+ features # probably would need rpi patches or smth anyways...
+
+# FYI current kernel: 6.6.31+rpt-rpi-2712
+
