36d9f9cb4a7d4b8c8b63b8a4d3fe56c99097e04f
Wes Higbee <wes.mcclure@gmail.com>
2024-09-08 14:33:35 -0500

    format yapf

--- vms/compile-rust/wes-userspace/scope.py
+++ vms/compile-rust/wes-userspace/scope.py
@@ -13,28 +13,24 @@ SAMPLE_INTERVAL = 0.5  # seconds
 samples = deque(maxlen=int(SAMPLE_DURATION / SAMPLE_INTERVAL))
 
 with gpiod.request_lines(
-    "/dev/gpiochip4",
-    consumer="test-wes",
-    config={
-        LINE: gpiod.LineSettings(
-            direction=Direction.OUTPUT, output_value=Value.ACTIVE
-        )
-    },
+        "/dev/gpiochip4",
+        consumer="test-wes",
+        config={LINE: gpiod.LineSettings(direction=Direction.OUTPUT, output_value=Value.ACTIVE)},
 ) as request:
     print(f"Initial value: {request.get_value(LINE)}")
-    
+
     start_time = time.time()
-    
+
     while True:
         current_time = time.time() - start_time
-        
+
         # Toggle GPIO value
         request.set_value(LINE, Value.ACTIVE)
         active_value = request.get_value(LINE)
         print(f"Active value: {active_value}")
         samples.append((current_time, 1 if active_value == Value.ACTIVE else 0))
         time.sleep(SAMPLE_INTERVAL)
-        
+
         request.set_value(LINE, Value.INACTIVE)
         inactive_value = request.get_value(LINE)
         print(f"Inactive value: {inactive_value}")
