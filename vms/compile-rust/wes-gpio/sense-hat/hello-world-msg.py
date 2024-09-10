37413bcdb30cd6a20217d9d3c3359dc5ebdf373d
Wes Higbee <wes.mcclure@gmail.com>
2024-09-10 01:08:20 -0500

    sense hat ex

---
+++ vms/compile-rust/wes-gpio/sense-hat/hello-world-msg.py
@@ -0,0 +1,20 @@
+# https://www.raspberrypi.com/products/sense-hat/
+# https://pythonhosted.org/sense-hat/
+from sense_hat import SenseHat
+
+sense = SenseHat()
+
+sense.show_message("Hello world!")
+
+# sudo python3  demo.py
+# must run as root (current permissions), or if not root:
+#   in .venv => ModuleNotFoundError: No module named 'RTIMU'
+#   in $HOME dir => Permission denied: '/dev/input/event5)
+#     ls -l /dev/input/event5 # root:input (owner:group)... can I add myself to input group to not need sudo?
+#     sudo usermod -aG input $USER
+#   => next =>
+#     Permission denied: '/dev/fb0'
+#     sudo usermod -aG video $USER
+#     PROFIT!!!!
+#        ASIDE:    sudo dmesg | grep fb0  =>  fb0: RPi-Sense FB frame buffer device
+#
