a7bb609bb40a6ad77cdaadc73c65235c7481dd8a
Wes Higbee <wes.mcclure@gmail.com>
2024-09-11 16:29:07 -0500

    notes

---
+++ vms/compile-rust/wes-gpio/dht22fs/dht22_io.c
@@ -0,0 +1,7 @@
+
+// IIO overview: https://wiki.analog.com/software/linux/docs/iio/iio
+// use iio-tri-sysfs to trigger sample aquisition https://wiki.analog.com/software/linux/docs/iio/iio-trig-sysfs
+//    use this to combine triggering and ring buffer (in software):
+//      iio_triggered_buffer_setup_ext
+//        takes iio_dev => makes iio_buffer
+
