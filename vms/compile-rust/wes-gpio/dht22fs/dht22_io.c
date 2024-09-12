f1e8a20d639de39f93c4678e9f3e202ac875d1c8
Wes Higbee <wes.mcclure@gmail.com>
2024-09-12 11:15:46 -0500

    port to gpiod since under hood gpio_ uses gpiod

--- vms/compile-rust/wes-gpio/dht22fs/dht22_io.c
+++ vms/compile-rust/wes-gpio/dht22fs/dht22_io.c
@@ -5,3 +5,7 @@
 //      iio_triggered_buffer_setup_ext
 //        takes iio_dev => makes iio_buffer
 
+
+// kernel docs for GPIO => entrypoint explains legacy vs current (gpiod) APIs
+// https://github.com/raspberrypi/linux/blob/rpi-6.8.y/Documentation/driver-api/gpio/intro.rst#L8
+// legacy absolute GPIO numbering API => https://github.com/raspberrypi/linux/blob/rpi-6.8.y/Documentation/driver-api/gpio/legacy.rst#L1
