03b1862d5aeb38308b5411e75b90d50d924c673f
Wes Higbee <wes.mcclure@gmail.com>
2024-09-13 00:25:31 -0500

    format

--- vms/compile-rust/wes-gpio/dht22/dht22iio.c
+++ vms/compile-rust/wes-gpio/dht22/dht22iio.c
@@ -36,16 +36,15 @@
 // https://github.com/raspberrypi/linux/blob/rpi-6.8.y/Documentation/driver-api/gpio/intro.rst#L8
 // legacy absolute GPIO numbering API => https://github.com/raspberrypi/linux/blob/rpi-6.8.y/Documentation/driver-api/gpio/legacy.rst#L1
 
-
 static int read_raw(struct iio_dev *iio_dev,
-                    struct iio_chan_spec const *chan,
-                    int *val,
-                    int *val2,
-                    long mask)
+										struct iio_chan_spec const *chan,
+										int *val,
+										int *val2,
+										long mask)
 {
-	PR_INFO("read_data: %d\n", chan->type); 
-	
-	struct dht22 *dht22 = iio_priv(iio_dev);	// IIO interface, so if you want other device info, have to look it up
+	PR_INFO("read_data: %d\n", chan->type);
+
+	struct dht22 *dht22 = iio_priv(iio_dev); // IIO interface, so if you want other device info, have to look it up
 
 	int ms_since_last_read = jiffies_to_msecs(jiffies - dht22->last_read_jiffies);
 	if (ms_since_last_read < 2000 && ms_since_last_read > 0)
@@ -63,7 +62,7 @@ static int read_raw(struct iio_dev *iio_dev,
 	PR_INFO("read_data: Data is stale, reading new data\n");
 
 	// mutex_lock(&dht11->lock); // TODO
-	
+
 	// TODO USE: dht22->gpio_desc
 
 	// mutex_unlock(&dht11->lock); // TODO (also goto err: ... like dht11 does)
@@ -76,27 +75,30 @@ static int read_raw(struct iio_dev *iio_dev,
 	else
 		return -EINVAL;
 
-	return IIO_VAL_INT; 
+	return IIO_VAL_INT;
 }
 
 static const struct iio_info dht22_iio_info = {
-    .read_raw = read_raw,
+		.read_raw = read_raw,
 };
-
 
 // each channel => /sys/bus/iio/devices/iio:deviceX/in_<type>_raw
 static const struct iio_chan_spec dht22_chan_spec[] = {
-	{ .type = IIO_TEMP,
-		.info_mask_separate = BIT(IIO_CHAN_INFO_PROCESSED), },
-	{ .type = IIO_HUMIDITYRELATIVE,
-		.info_mask_separate = BIT(IIO_CHAN_INFO_PROCESSED), }
-};
+		{
+				.type = IIO_TEMP,
+				.info_mask_separate = BIT(IIO_CHAN_INFO_PROCESSED),
+		},
+		{
+				.type = IIO_HUMIDITYRELATIVE,
+				.info_mask_separate = BIT(IIO_CHAN_INFO_PROCESSED),
+		}};
 
-struct dht22 {
-  struct iio_dev *indio_dev;
-  struct device *dev;
-  struct gpio_desc *gpio_desc;
-  int irq;
+struct dht22
+{
+	struct iio_dev *indio_dev;
+	struct device *dev;
+	struct gpio_desc *gpio_desc;
+	int irq;
 
 	int celsius_tenths;
 	int humidity_tenths;
@@ -111,28 +113,29 @@ static int dht22_probe(struct platform_device *pdev)
 	struct iio_dev *iio;
 
 	iio = devm_iio_device_alloc(dev, sizeof(*dht22)); // also links dht22 to iio device, can retrieve later on in read (above)
-	if (!iio) {
+	if (!iio)
+	{
 		dev_err(dev, "Failed to allocate IIO device\n");
 		return -ENOMEM;
 	}
 
-  dht22 = iio_priv(iio); // retrieve allocated memory for private data (dht22) from call to devm_iio_device_alloc (above)
-  dht22->dev = dev; // store platform device pointer
-  dht22->gpio_desc = devm_gpiod_get(dev, NULL, GPIOD_IN); // get GPIO descriptor from device tree (i.e. /boot/firmware/config.txt on rpi)
+	dht22 = iio_priv(iio);																	// retrieve allocated memory for private data (dht22) from call to devm_iio_device_alloc (above)
+	dht22->dev = dev;																				// store platform device pointer
+	dht22->gpio_desc = devm_gpiod_get(dev, NULL, GPIOD_IN); // get GPIO descriptor from device tree (i.e. /boot/firmware/config.txt on rpi)
 	if (IS_ERR(dht22->gpio_desc))
 		return PTR_ERR(dht22->gpio_desc);
 
-  // TODO rewrite to be IRQ triggered later on (after port to IIO)
-  // dht22->irq = gpiod_to_irq(dht22->gpio_desc);
-  // if (dht22->irq < 0) {
-  //   dev_err(dev, "GPIO %d has no interrupt\n", desc_to_gpio(dht22->gpio_desc));
-  //   return -EINVAL;
-  // }
+	// TODO rewrite to be IRQ triggered later on (after port to IIO)
+	// dht22->irq = gpiod_to_irq(dht22->gpio_desc);
+	// if (dht22->irq < 0) {
+	//   dev_err(dev, "GPIO %d has no interrupt\n", desc_to_gpio(dht22->gpio_desc));
+	//   return -EINVAL;
+	// }
 
-  // TODO, I don't think this is needed:
-  // platform_set_drvdata(pdev, iio);
+	// TODO, I don't think this is needed:
+	// platform_set_drvdata(pdev, iio);
 
-	// init_completion(&dht22->completion); // 
+	// init_completion(&dht22->completion); //
 	// mutex_init(&dht22->lock); // TODO lock on read (one at a time)
 	iio->name = pdev->name;
 	iio->info = &dht22_iio_info;
@@ -144,19 +147,20 @@ static int dht22_probe(struct platform_device *pdev)
 }
 
 static const struct of_device_id dht22_dt_ids[] = {
-	{ .compatible = "dht22", },
-	{ }
-};
+		{
+				.compatible = "dht22",
+		},
+		{}};
 MODULE_DEVICE_TABLE(of, dht22_dt_ids);
 
-#define DRIVER_NAME	"dht22"
+#define DRIVER_NAME "dht22"
 
 static struct platform_driver dht22_driver = {
-	.driver = {
-		.name	= DRIVER_NAME,
-		.of_match_table = dht22_dt_ids,
-	},
-	.probe  = dht22_probe,
+		.driver = {
+				.name = DRIVER_NAME,
+				.of_match_table = dht22_dt_ids,
+		},
+		.probe = dht22_probe,
 };
 
 module_platform_driver(dht22_driver); // instead of module_init and module_exit, for simple drivers
