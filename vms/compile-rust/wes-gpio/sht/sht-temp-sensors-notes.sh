fb65e6488fa782575b16cd24030eb319e875f5c5
Wes Higbee <wes.mcclure@gmail.com>
2024-09-14 23:45:09 -0500

    notes

---
+++ vms/compile-rust/wes-gpio/sht/sht-temp-sensors-notes.sh
@@ -0,0 +1,6 @@
+
+# https://learn.adafruit.com/adafruit-sht40-temperature-humidity-sensor?view=all
+# sht4x (own kernel driver already)
+# sht3x, shtc1, sht15, sht21,
+# PRN get a sht4x sensor just to try it (after I conquer others that I have)
+#   maybe look into i2c over USB (like ft232 does UART over USB - ftdi IIRC)
