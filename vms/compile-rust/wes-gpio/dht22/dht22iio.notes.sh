786ca1da6da2dd8895dbabefda2ba8294a21de5a
Wes Higbee <wes.mcclure@gmail.com>
2024-09-15 00:35:06 -0500

    w00t... dht22iio overlay+driver loads on boot for both sensors

--- vms/compile-rust/wes-gpio/dht22/dht22iio.notes.sh
+++ vms/compile-rust/wes-gpio/dht22/dht22iio.notes.sh
@@ -38,8 +38,28 @@ sudo modprobe industrialio # modprobe has search paths to load deps
 sudo insmod dht22iio.ko # now, this works ... remember insmod literally just loads the module you pass, no search paths
 
 ## put in /boot/firmeware/config.txt:
-# dtoverlay=dht22iio,gpiopin=17
-# dtoverlay=dht22iio,gpiopin=27
+# dtoverlay=dht22iio,gpio_pin=17
+# dtoverlay=dht22iio,gpio_pin=27
 #
 # seems to ignore if overlay not found as I rebooted before `make install_driver`
-# TODO TESTING
+#
+# manual load before reboot:
+sudo modprobe industrialio   # must load dep until I install_driver to modprobe mine
+sudo insmod dht22iio.ko
+sudo dtoverlay dht22iio.dtbo gpio_pin=17
+cat /sys/bus/iio/devices/iio:device*/in_humidityrelative_input
+# YASSSSSS!!!
+sudo dtoverlay dht22iio.dtbo gpio_pin=27
+# YAY both there ... # ! TODO now I need to address the timing issues... most of time one fails... a few times both responded
+make install_driver
+make status_driver
+sudo reboot
+# didn't work after first reboot... so I troubleshooted:
+# FOUND dtoverlay had dht22iio (IIRC) so I moved on to troubleshoot dht22iio.ko (module/driver)
+sudo modprobe dht22iio # failed... said not in /lib/modules... when it was... so I ran:
+sudo depmod -a
+sudo modprobe dht22iio # loaded! and /boot/firmware/config.txt overlays kicked in for both DHT22 sensors!!!!
+ls /sys/bus/iio/devices/iio:device*/in_temp_input # w00h00
+cat /sys/bus/iio/devices/iio:device0/in_temp_input # yay!
+sudo reboot
+# YAYAYAYA they are both there after reboot
