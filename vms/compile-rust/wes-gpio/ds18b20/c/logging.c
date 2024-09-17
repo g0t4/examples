6b9d5dfa4cdd5dfe6813f2a9d8d1618d35bf318b
Wes Higbee <wes.mcclure@gmail.com>
2024-09-17 00:34:23 -0500

    logging

---
+++ vms/compile-rust/wes-gpio/ds18b20/c/logging.c
@@ -0,0 +1,11 @@
+#if LOG_LEVEL >= 1
+#define LOG_INFO(fmt, ...) printf("INFO: " fmt "\n", ##__VA_ARGS__)
+#else
+#define LOG_INFO(fmt, ...)
+#endif
+
+#if LOG_LEVEL >= 2
+#define LOG_DEBUG(fmt, ...) printf("DEBUG: " fmt "\n", ##__VA_ARGS__)
+#else
+#define LOG_DEBUG(fmt, ...)
+#endif
