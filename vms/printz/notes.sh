41d262b2b1a2b295b0153eba0cabe0a30d91ba28
Wes Higbee <wes.mcclure@gmail.com>
2024-09-29 19:25:18 -0500

    notes

--- vms/printz/notes.sh
+++ vms/printz/notes.sh
@@ -1,4 +1,5 @@
 # notes copying guestvm to victim (vm)
+# shutdown guestvm and duplicate its disk (probably could do some sort of snapshotting and not need a full clone but meh for now, this way they are fully independent if I wanna nuke one and not both)
 
 virsh define victim.xml
 virsh start victim # -> took over guestvm dhcp lease (b/c was cached + client id same)
