719cd702d5a7cb5c8664bc8422ab8ecfb796b9b2
Wes Higbee <wes.mcclure@gmail.com>
2025-02-02 17:44:57 -0600

    4

--- rocm/ggml-exercise/sizes/sizes.cpp
+++ rocm/ggml-exercise/sizes/sizes.cpp
@@ -1,34 +1,36 @@
 #include <iostream>
 
 enum Color {
-  red,
-  green,
-  blue
+    red,
+    green,
+    blue
 };
 enum Color8 : uint8_t {
-  red8,
-  green8,
-  blue8
+    red8,
+    green8,
+    blue8
 };
 
 int main() {
 
-  std::cout << "The size of an int is: " << sizeof(int) << std::endl;
-  std::cout << "The size of a short int is: " << sizeof(short int) << std::endl;
+    std::cout << "The size of an int is: " << sizeof(int) << std::endl;
+    std::cout << "The size of a short int is: " << sizeof(short int) << std::endl;
 
-  std::cout << "The size of a long int is: " << sizeof(long int) << std::endl;
+    std::cout << "The size of a long int is: " << sizeof(long int) << std::endl;
 
-  std::cout << "The size of a long long int is: " << sizeof(long long int) << std::endl;
+    std::cout << "The size of a long long int is: " << sizeof(long long int) << std::endl;
 
-  std::cout << "The size of a double is: " << sizeof(double) << std::endl;
+    std::cout << "The size of a double is: " << sizeof(double) << std::endl;
 
-  // char type is signed by default
-  std::cout << "The size of a char is: " << sizeof(char) << std::endl;
+    // char type is signed by default
+    std::cout << "The size of a char is: " << sizeof(char) << std::endl;
 
-  // unsigned data types are
+    // unsigned data types are
 
-  std::cout << "and an enum is : " << sizeof(Color) << std::endl;
-  std::cout << "and uint8_t enum is : " << sizeof(Color8) << std::endl;
+    std::cout << "and an enum is : " << sizeof(Color) << std::endl;
+    std::cout << "and uint8_t enum is : " << sizeof(Color8) << std::endl;
 
-  return 0;
+
+
+    return 0;
 }
