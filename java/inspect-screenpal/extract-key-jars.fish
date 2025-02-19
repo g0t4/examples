f92a32f8dd54fbd392c0e52cd67c32e057d83289
Wes Higbee <wes.mcclure@gmail.com>
2025-02-19 10:23:40 -0600

    parameterize version

--- java/inspect-screenpal/extract-key-jars.fish
+++ java/inspect-screenpal/extract-key-jars.fish
@@ -1,13 +1,17 @@
 #!/usr/bin/env fish
 
-echo "wipe dir if wanna start from scratch"
-echo "    trash ~/repos/scratch/extract-screenpal"
+# TODO UPDATE VERSION as needed
+set screenpal_version "v3.16.0"
+echo "extracting version: $screenpal_version"
+
+# echo "wipe dir if wanna start from scratch"
+# echo "    trash ~/repos/scratch/extract-screenpal"
 
 mkdir -p ~/repos/scratch/extract-screenpal/app
-mkdir -p ~/repos/scratch/extract-screenpal/Library
+mkdir -p ~/repos/scratch/extract-screenpal/$screenpal_version
 
 cp /Applications/ScreenPal.app/Contents/app/AppMain-3.1.10.jar ~/repos/scratch/extract-screenpal/app
-cp ~/Library/ScreenPal-v3/v3.16.0/*.jar ~/repos/scratch/extract-screenpal/Library
+cp ~/Library/ScreenPal-v3/$screenpal_version/*.jar ~/repos/scratch/extract-screenpal/$screenpal_version
 
 # app
 # -o = overwrite existing files (otherwise it prompts)
@@ -16,6 +20,6 @@ for jar in ~/repos/scratch/extract-screenpal/app/*.jar
 end
 
 # lib
-for jar in ~/repos/scratch/extract-screenpal/Library/*.jar
+for jar in ~/repos/scratch/extract-screenpal/$screenpal_version/*.jar
     unzip -o -d $jar-expand $jar
 end
