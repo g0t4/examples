3821d6b1771423297853ca9be9c864e7fb514af1
Wes Higbee <wes.mcclure@gmail.com>
2025-02-19 10:16:21 -0600

    expand jars too

--- java/inspect-screenpal/extract-key-jars.fish
+++ java/inspect-screenpal/extract-key-jars.fish
@@ -8,3 +8,14 @@ mkdir -p ~/repos/scratch/extract-screenpal/Library
 
 cp /Applications/ScreenPal.app/Contents/app/AppMain-3.1.10.jar ~/repos/scratch/extract-screenpal/app
 cp ~/Library/ScreenPal-v3/v3.16.0/*.jar ~/repos/scratch/extract-screenpal/Library
+
+# app
+# -o = overwrite existing files (otherwise it prompts)
+for jar in ~/repos/scratch/extract-screenpal/app/*.jar
+    unzip -o -d $jar-expand $jar
+end
+
+# lib
+for jar in ~/repos/scratch/extract-screenpal/Library/*.jar
+    unzip -o -d $jar-expand $jar
+end
