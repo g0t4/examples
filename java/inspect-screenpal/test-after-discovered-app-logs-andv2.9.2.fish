d8057176d1c70936b8f07aa03a1ddbd18525341c
Wes Higbee <wes.mcclure@gmail.com>
2025-02-19 02:06:09 -0600

    notes

--- java/inspect-screenpal/test-after-discovered-app-logs-andv2.9.2.fish
+++ java/inspect-screenpal/test-after-discovered-app-logs-andv2.9.2.fish
@@ -24,6 +24,16 @@ export APPDIR=/Applications/ScreenPal.app/Contents/app
 #        ok 2.9.2. is what is suggested for download by this script (when this works, after remove 2.9.2 this then works again)
 # ***! TRYING TO USE swexpl TRIGGERED THE DOWNLOAD of 2.9.2
 #        FYI app-0.log => you can see args aren't passed correctly... one has URL w/ comman delimited args in URL?!
+#           major diff (run .app vs swexpl => INFO T0001: Running with args: [] ...
+#              when i run with test.fish & with app regular luanch ... nothing in args list
+#              when I try to use via swexpl... then args show in that list... ... fix this and it wil likely start from swexpl... 
+#                  smth with how agent launches the app vs w/o agent (IIUC same issue with intellij to launch it run config)
+
+#           args seem ok actually in log
+#              url is messed up but ...
+#              this is error:
+#                 02/19/25 01:48:46 INFO T0022: UPDATING-STARTING-UP-FRAME: The Splash Screen was not showing.  (The user has not run the newer ScreenPal Installer.)  Opening the Updating/Starting-Up identical looking image of (656, 354, 608, 373).
+# ***             No current version found. => hence download 2.9.2
 #           Checking previous app port: http://127.0.0.1:56480/-Djpackage.app-version%3D3,--add-opens,java.desktop%2Fjava.awt%3DALL-UNNAMED,--add-opens,java.desktop%2Fjava.awt.event%3DALL-UNNAMED,--add-opens,java.desktop%2Fjava.awt.peer%3DALL-UNNAMED,--add-opens,java.desktop%2Fjavax.swing%3DALL-UNNAMED,--add-opens,java.desktop%2Fjavax.swing.plaf.basic%3DALL-UNNAMED,--add-opens,java.desktop%2Fjavax.swing.text%3DALL-UNNAMED,--add-opens,java.desktop%2Fsun.awt%3DALL-UNNAMED,--add-opens,java.desktop%2Fsun.lwawt%3DALL-UNNAMED,--add-opens,java.desktop%2Fsun.lwawt.macosx%3DALL-UNNAMED,-splash%3A%2FApplications%2FScreenPal.app%2FContents%2Fapp%2FScreenPalSplashScreen.png,-Dapple.awt.enableTemplateImages%3Dtrue,-Djava.system.class.loader%3DDynamicURLClassLoader,-Dsom.exe.path%3D%2FApplications%2FScreenPal.app%2FContents%2Fapp%2FXXXNOEXE,-Dsom.mac.app%3D%2FApplications%2FScreenPal.app%2FContents%2Fapp%2F..%2F..,-Dsom.mac.app.launcher%3D%2FApplications%2FScreenPal.app%2FContents%2Fapp%2FScreenPal.app,-Dsom.mac.app.tray%3D%2FApplications%2FScreenPal.app%2FContents%2Fapp%2FScreenPalTray.app,-Dsp.installer.version%3D3.0.2.0,-Dsun.java2d.opengl%3Dtrue,-Xmx2G
 #           as if it is checking for a new version from this location... what are the args to pass? in what order or?
 # !!! ARGS are likely messed up still in swexpl case
