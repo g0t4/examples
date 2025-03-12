296869194afa72730422e0c8457ff8e6d4b5f366
Wes Higbee <wes.mcclure@gmail.com>
2025-03-12 08:16:21 -0500

    notes

---
+++ 5090/try-exllamav2/notes.fish
@@ -0,0 +1,7 @@
+# TODO try:
+#  https://github.com/theroyallab/tabbyAPI?tab=readme-ov-file
+#  which uses the Exllamav2 backend
+#  
+#  so far I installed the backend package only, was gonna test it directly...
+
+# TODO gonna have to make sure I use nightly 12.8 torch packages too
