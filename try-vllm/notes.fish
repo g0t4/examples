e1c4ff022903954719acc9d9b24cd6d80543f83f
Wes Higbee <wes.mcclure@gmail.com>
2025-03-12 08:04:18 -0500

    notes

--- try-vllm/notes.fish
+++ try-vllm/notes.fish
@@ -1,1 +1,4 @@
 pip3 install --pre torch torchvision torchaudio --index-url https://download.pytorch.org/whl/nightly/cu128
+
+# issue tracking 5090 support:
+# https://github.com/pytorch/pytorch/issues/145949
