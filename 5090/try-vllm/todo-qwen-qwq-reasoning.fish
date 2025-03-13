fee0029dfa6a2446cd3f9feb2d54e4d789961145
Wes Higbee <wes.mcclure@gmail.com>
2025-03-13 01:47:34 -0500

    notes

---
+++ 5090/try-vllm/todo-qwen-qwq-reasoning.fish
@@ -0,0 +1,10 @@
+# QwQ https://huggingface.co/collections/Qwen/qwq-674762b79b75eac01735070a
+# maybe AWQ:
+#   https://huggingface.co/Qwen/QwQ-32B-AWQ/tree/main
+# else GGUF:
+#   https://huggingface.co/Qwen/QwQ-32B-GGUF/tree/main
+
+# QVQ!
+# wont fit most likely but visual models for reasoning?!
+#   https://huggingface.co/collections/Qwen/qvq-676448c820912236342b9888
+#     https://huggingface.co/Qwen/QVQ-72B-Preview
