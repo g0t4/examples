4b053f84af1e4173086ed0604a9f8fbec29de14e
Wes Higbee <wes.mcclure@gmail.com>
2025-04-10 17:39:58 -0500

    write to file so I have some refs

--- 5090/try-vllm/graph-hf-model.py
+++ 5090/try-vllm/graph-hf-model.py
@@ -1,3 +1,4 @@
+import sys
 import base64
 from IPython.display import Image, display
 from transformers import AutoModelForCausalLM, AutoModel, AutoTokenizer
@@ -16,7 +17,12 @@ def show_image_iterm2(path):
 # device='meta' -> no memory is consumed for visualization
 
 # model_name = "meta-llama/Llama-3.2-1B-Instruct" # waiting for approval to gated
-model_name = "Qwen/Qwen2.5-Coder-0.5B"
+model_name ="Qwen/Qwen2.5-Coder-0.5B"
+is_ipython = sys.argv[0].endswith("ipython")
+if not is_python and len(sys.argv) > 1:
+    model_name = sys.argv[1]
+print(model_name)
+
 tokenizer = AutoTokenizer.from_pretrained(model_name)
 model = AutoModel.from_pretrained(model_name)
 
@@ -25,9 +31,18 @@ model = AutoModel.from_pretrained(model_name)
 from torchinfo import summary
 
 # *** VERY NICE SUMMARY! (text and tree like)
-summary(model, input_size=(1, 32), dtypes= [ torch.long ] )  # batch size 1, seq len 32
+model_stats = summary(model, input_size=(1, 32), dtypes= [ torch.long ] )  # batch size 1, seq len 32
+print(model_stats)
 
+#%% 
 
+# write to model-graphs/ dir using model name santized
+# model_graph = draw_graph(model, input_data=input_ids, expand_nested=True) 
+file_name = model_name.replace('/', '-')
+file_path = f"model-graphs/{file_name}.txt"
+what = str(model_stats)
+with open(file_path, 'wb') as f:
+    f.write(what.encode('utf8'))
 
 
 # OTHER TRIALS before using torchinfo which is all I really wanted (so far)
