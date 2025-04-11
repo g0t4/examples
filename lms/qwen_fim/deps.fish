ac0101d3facfbea85164798c39e2ef53014aef58
Wes Higbee <wes.mcclure@gmail.com>
2025-04-11 10:32:32 -0500

    ex from qwen repo for repo level FIM

---
+++ lms/qwen_fim/deps.fish
@@ -0,0 +1,10 @@
+uv venv
+uv pip install transformers ipython accelerate
+uv pip install --pre torch torchvision torchaudio --index-url https://download.pytorch.org/whl/nightly/cu128
+
+#%% 
+
+uv pip list
+
+#%% 
+
