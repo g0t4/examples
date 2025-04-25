0a188b6cf1a29f09edb8055941683c3bc39a912a
Wes Higbee <wes.mcclure@gmail.com>
2025-04-25 03:19:30 -0500

    test script is working

--- 5090/try-zeta/server/test.sh
+++ 5090/try-zeta/server/test.sh
@@ -1,7 +1,6 @@
 #!/usr/bin/env bash
 
-# http localhost:9000/predict_edits <<'EOF'
-jq . <<'EOF'
+http localhost:9000/predict_edits <<'EOF'
 {
     "outline": "```lua/ask-openai/prediction/tests/calc/calc.lua\nfunction M.add\n```\n",
     "input_events": "User edited \"lua/ask-openai/prediction/tests/calc/calc.lua\":\n```diff\n@@ -7,4 +7,5 @@\n \n \n \n+\n return M\n\n```\n\nUser edited \"lua/ask-openai/prediction/tests/calc/calc.lua\":\n```diff\n@@ -8,4 +8,5 @@\n \n \n \n+\n return M\n\n```",
@@ -11,3 +10,11 @@ jq . <<'EOF'
     "diagnostic_groups": []
 }
 EOF
+
+
+# FYI should return smth like:
+# {
+#     "output_excerpt": "<|editable_region_start|>\nlocal M = {}\n\nfunction M.add(a, b)\n    return a + b\nend\n\nfunction M.subtract(a, b)\n    return a - b\nend\n\nfunction M.multiply(a, b)\n    return a * b\nend\n\nfunction M.divide(a, b)\n    if b == 0 then\n        error(\"Division by zero\")\n    end\n    return a / b\nend\n\nreturn M\n\n<|editable_region_end|>\n```\n",
+#     "request_id": "7914a580d7184a039ec30e23d74c8c37"
+# }
+
