6af3e9a35feb2419ad25fb6355096625b8333e03
Wes Higbee <wes.mcclure@gmail.com>
2025-05-30 01:05:26 -0500

    notes to run on cpu

--- lms/test-chatterbox/main.py
+++ lms/test-chatterbox/main.py
@@ -2,6 +2,13 @@ import torchaudio as ta
 from chatterbox.tts import ChatterboxTTS
 
 model = ChatterboxTTS.from_pretrained(device="cuda")
+
+# ** cpu notes (MUCH MUCH SLOWER like 30 seconds vs 1 second on mbp with m1 max)
+# actually CPU works fine if you go in and modify torch.load calls to pass map_location="cpu"... even though they have .to(device) calls... that's not enough
+#  go to this function:
+#    https://github.com/g0t4/examples/blob/2aaa636/lms/test-chatterbox/.venv/lib/python3.12/site-packages/chatterbox/tts.py#L126
+#    torch.load(ckpt_dir / "ve.pt", map_location="cpu")
+#    and 4 more torch.loads after this (worked for me)... I did not verify why or which ones actually need it, just noting it worked
 # model = ChatterboxTTS.from_pretrained(device="cpu")
 
 # text = "Ezreal and Jinx teamed up with Ahri, Yasuo, and Teemo to take down the enemy's Nexus in an epic late-game pentakill."
