3a3c126226f8bc43ad751ba692f7352b7e6702a8
Wes Higbee <wes.mcclure@gmail.com>
2025-05-30 01:58:46 -0500

    new example, with 10s and full reference clips to test result

--- lms/test-chatterbox/main.py
+++ lms/test-chatterbox/main.py
@@ -4,13 +4,16 @@ from chatterbox.tts import ChatterboxTTS
 model = ChatterboxTTS.from_pretrained(device="cuda")
 # model = ChatterboxTTS.from_pretrained(device="cpu")
 
-text = "Ezreal and Jinx teamed up with Ahri, Yasuo, and Teemo to take down the enemy's Nexus in an epic late-game pentakill."
+# text = "Ezreal and Jinx teamed up with Ahri, Yasuo, and Teemo to take down the enemy's Nexus in an epic late-game pentakill."
+
+text = "Let me tell you about chatterbox-tts, a python package wrapping a new model, based on llama 0.5B, that can take a reference audio file with your own voice, plus a text prompt, and it will generate an audio file with your voice speaking the text part, and it is fast with a CUDA device."
 wav = model.generate(text)
-ta.save("tmp/output/test1.wav", wav, model.sr)
+ta.save("tmp/output/test3-01-stock-voice.wav", wav, model.sr)
 
-print("doing me now")
-
-# If you want to synthesize with a different voice, specify the audio prompt
-AUDIO_PROMPT_PATH="tmp/part2-10s.wav"
+AUDIO_PROMPT_PATH="tmp/my-voice-reference-10s.wav"
 wav = model.generate(text, audio_prompt_path=AUDIO_PROMPT_PATH)
-ta.save("tmp/output/test1-me.wav", wav, model.sr)
+ta.save("tmp/output/test3-02-me-from-10s-clip.wav", wav, model.sr)
+
+AUDIO_PROMPT_PATH="tmp/my-voice-reference-full.wav"
+wav = model.generate(text, audio_prompt_path=AUDIO_PROMPT_PATH)
+ta.save("tmp/output/test3-02-me-from-full-clip.wav", wav, model.sr)
