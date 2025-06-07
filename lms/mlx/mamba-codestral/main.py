f79d02b9f4acbb610106ca8505988b53fcc5aeb1
Wes Higbee <wes.mcclure@gmail.com>
2025-06-07 02:16:17 -0500

    example mamba-codestral

--- lms/mlx/mamba-codestral/main.py
+++ lms/mlx/mamba-codestral/main.py
@@ -1,6 +1,18 @@
-def main():
-    print("Hello from mamba-codestral!")
+from mlx_lm import load, generate
+
+model, tokenizer = load("mlx-community/Mamba-Codestral-7B-v0.1-8bit")
+
+#%% 
+
+prompt = "hello"
 
 
-if __name__ == "__main__":
-    main()
+if tokenizer.chat_template is not None:
+    messages = [{"role": "user", "content": prompt}]
+    prompt = tokenizer.apply_chat_template(
+        messages, add_generation_prompt=True
+    )
+#%%
+
+response = generate(model, tokenizer, prompt=prompt, verbose=True)
+
