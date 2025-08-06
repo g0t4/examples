69a29d9e0019b5c39ddaaef5b62cfcbeda94d2b3
Wes Higbee <wes.mcclure@gmail.com>
2025-08-05 19:30:20 -0500

    look for several diff tokens

--- lms/gptoss/tokens.py
+++ lms/gptoss/tokens.py
@@ -2,8 +2,8 @@ import re
 from transformers import AutoTokenizer
 
 # Load tokenizer (adjust model path if needed)
-# tokenizer = AutoTokenizer.from_pretrained("openai/gpt-oss-20b")
-tokenizer = AutoTokenizer.from_pretrained("Qwen/Qwen2.5-Coder-7B")
+tokenizer = AutoTokenizer.from_pretrained("openai/gpt-oss-20b")
+# tokenizer = AutoTokenizer.from_pretrained("Qwen/Qwen2.5-Coder-7B")
 
 print(f"{tokenizer.vocab_size=}")
 print(f"{len(tokenizer)=}")
@@ -14,10 +14,20 @@ print(f"{real_vocab_size=}")
 print(tokenizer.special_tokens_map)
 print(tokenizer.additional_special_tokens)
 
-for num in range(real_vocab_size):
-    token = tokenizer.convert_ids_to_tokens(num)
-    if re.search(r'fim', token, re.IGNORECASE):
-        print(token)
+def search_for(search_regex):
+    for num in range(real_vocab_size):
+        token = tokenizer.convert_ids_to_tokens(num)
+        if re.search(search_regex, token, re.IGNORECASE):
+            print(token)
+
+search_for(r'fim')
+search_for(r'suffix')
+search_for(r'prefix')
+search_for(r'middle')
+
+# print last X tokens from end of list (past vocab_size)
+for i in range(tokenizer.vocab_size, real_vocab_size): 
+    print(f"{i}: " + tokenizer.convert_ids_to_tokens(i))
 
 look_for_tokens = ["<|fim_prefix|>", "<|fim_middle|>", "<|fim_suffix|>"]
 look_for_ids = tokenizer.convert_tokens_to_ids(look_for_tokens)
