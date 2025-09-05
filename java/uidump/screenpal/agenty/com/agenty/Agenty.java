41c87f33d784597a98560b39af507483e4914857
Wes Higbee <wes.mcclure@gmail.com>
2025-09-05 17:42:58 -0500

    setTitle works!!!!

--- java/uidump/screenpal/agenty/com/agenty/Agenty.java
+++ java/uidump/screenpal/agenty/com/agenty/Agenty.java
@@ -36,6 +36,22 @@ public final class Agenty {
     private static void dump(Component c, int depth) {
         String pad = "  ".repeat(depth);
         String title = (c instanceof Frame f) ? f.getTitle() : (c instanceof Dialog d) ? d.getTitle() : null;
+        System.out.printf("JERKWAD '%s'", title);
+        if (title != null && title.contains("ScreenPal - 3.19.4")) {
+            // FYI original title "\nScreenPal - 3.19.4\n" had new lines (or smth off)
+            // before and after it! so use contains
+            System.out.println("FOUND YOU FUUUER");
+            if (c instanceof Frame f) {
+                System.out.println("  fuuuu is a frame");
+                f.setTitle("FUUUU FRAME");
+            }
+
+            if (c instanceof Dialog d) {
+                System.out.println("  fuuuu is a dialog");
+                d.setTitle("FUUUU DIALOG");
+            }
+        }
+
         var ac = (c instanceof Accessible a) ? a.getAccessibleContext() : null;
         System.out.printf("[agenty]%s%s bounds=%s visible=%s name=%s accName=%s%n",
                 pad, c.getClass().getName(), c.getBounds(), c.isVisible(),
