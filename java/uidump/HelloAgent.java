7301b8bde982fffbeac226fc901a0d171a91623d
Wes Higbee <wes.mcclure@gmail.com>
2025-09-04 23:16:31 -0500

    add println to hello agent load

--- java/uidump/HelloAgent.java
+++ java/uidump/HelloAgent.java
@@ -1,14 +1,17 @@
-// HelloAgent.java  (Manifest: Agent-Class: HelloAgent)
 import java.io.*; import java.lang.instrument.*; import java.nio.file.*; 
 public class HelloAgent {
-  public static void agentmain(String args, Instrumentation inst) {
-    try {
-      String pid = java.lang.management.ManagementFactory.getRuntimeMXBean().getName().split("@")[0];
-      Files.writeString(Paths.get("agent-ok-" + pid + ".txt"),
-        "hello from agent; args=" + args + "\n");
-    } catch (Throwable t) {
-      try { Files.writeString(Paths.get("agent-err.txt"), t.toString()); } catch (Exception ignored) {}
-      throw new RuntimeException(t);
+    public static void agentmain(String args, Instrumentation inst) {
+        try {
+
+            System.out.println("hello");
+
+
+            String pid = java.lang.management.ManagementFactory.getRuntimeMXBean().getName().split("@")[0];
+            Files.writeString(Paths.get("agent-ok-" + pid + ".txt"),
+                "hello from agent; args=" + args + "\n");
+        } catch (Throwable t) {
+            try { Files.writeString(Paths.get("agent-err.txt"), t.toString()); } catch (Exception ignored) {}
+            throw new RuntimeException(t);
+        }
     }
-  }
 }
