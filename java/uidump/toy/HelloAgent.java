6e45c084eb0a7f524258749c06ede3cd6f202a3c
Wes Higbee <wes.mcclure@gmail.com>
2025-09-05 00:03:13 -0500

    testing path issues

--- java/uidump/toy/HelloAgent.java
+++ java/uidump/toy/HelloAgent.java
@@ -1,16 +1,19 @@
 // HelloAgent.java  (NO package)
 import java.io.*; import java.lang.instrument.*; import java.nio.file.*;
 public class HelloAgent {
-  public static void premain(String args, Instrumentation inst) { marker("premain", args); }
-  public static void agentmain(String args, Instrumentation inst) { marker("agentmain", args); }
-  private static void marker(String where, String args) {
-    System.err.println("[SUCK IT BITCHES] " + where);  // see "grep -r SUCK IT" in this repo
-    try {
-      String pid = java.lang.management.ManagementFactory.getRuntimeMXBean().getName().split("@")[0];
-      Files.writeString(Paths.get("agent-ok-" + pid + ".txt"),
-        where + " args=" + args + "\n", java.nio.file.StandardOpenOption.CREATE,
-        java.nio.file.StandardOpenOption.APPEND);
-      System.err.println("[HelloAgent] " + where + " loaded");
-    } catch (Throwable t) { t.printStackTrace(); }
-  }
+    public static void agentmain(String args, Instrumentation inst) {
+        String pid = java.lang.management.ManagementFactory.getRuntimeMXBean().getName().split("@")[0];
+        String tmp = System.getProperty("java.io.tmpdir");
+        log(tmp + "agent-ok-"+pid+".txt", "ok");
+        System.err.println("[HelloAgent] loaded pid=" + pid + " tmp=" + tmp);
+    }
+    static void log(String p, String s){
+        try { java.nio.file.Files.writeString(java.nio.file.Path.of(p), s+"\n",
+            java.nio.file.StandardOpenOption.CREATE, java.nio.file.StandardOpenOption.APPEND); }
+        catch(Exception e){ System.err.println("WRITE FAIL "+p+": "+e); }
+    }
+
+    public static void premain(String args, Instrumentation inst) { 
+        System.err.println("[HelloAgent] premain");
+    }
 }
