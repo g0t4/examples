72b6a827b3e7d991f62b6fa8af3e57d1103a6115
Wes Higbee <wes.mcclure@gmail.com>
2025-09-30 09:14:19 -0500

    tokenizer

--- models/test-bm25/main.py
+++ models/test-bm25/main.py
@@ -1,5 +1,15 @@
+import bm25s
+
 def main():
-    print("Hello from test-bm25!")
+    corpus = [
+        "a cat is a feline and likes to purr",
+        "a dog is the human's best friend and loves to play",
+        "a bird is a beautiful animal that can fly",
+    ]
+
+    corpus_tokens = bm25s.tokenize(corpus)
+    retriever = bm25s.BM25(corpus=corpus)
+    retriever.index(corpus_tokens)
 
 
 if __name__ == "__main__":
