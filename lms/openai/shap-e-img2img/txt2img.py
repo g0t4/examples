46eb85c077a48b4e18ee831686214f7fdd18084c
Wes Higbee <wes.mcclure@gmail.com>
2025-10-29 11:51:22 -0500

    get it working

--- lms/openai/shap-e-img2img/txt2img.py
+++ lms/openai/shap-e-img2img/txt2img.py
@@ -4,7 +4,9 @@ from diffusers.utils import export_to_gif
 
 
 ckpt_id = "openai/shap-e"
-pipe = ShapEPipeline.from_pretrained(repo).to("cuda")
+pipe = ShapEPipeline.from_pretrained(ckpt_id).to("cuda")
+
+# %% 
 
 
 guidance_scale = 15.0
@@ -13,7 +15,12 @@ images = pipe(
     prompt,
     guidance_scale=guidance_scale,
     num_inference_steps=64,
-    size=256,
+    frame_size=256,
 ).images
 
-gif_path = export_to_gif(images, "shark_3d.gif")
+
+# %% 
+
+for index, img_set in enumerate(images):
+    gif_path = export_to_gif(img_set, f"shark_sampled_3d_{index}.gif")
+
