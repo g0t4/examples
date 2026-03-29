bd6909cb0ee026de8f88a8fc25065193bac46678
Wes Higbee <wes.mcclure@gmail.com>
2026-03-29 17:22:08 -0500

    change to MiB

--- models/qwen3.5/qwen3omni/list_cuda_devices.py
+++ models/qwen3.5/qwen3omni/list_cuda_devices.py
@@ -20,12 +20,12 @@ def list_cuda_devices():
         print(f"{'':>3}  {'Capability:':<30}  {capability[0]}.{capability[1]}")
 
         memory_info = torch.cuda.memory.mem_get_info(idx)
-        free_memory_gb = memory_info[0] / (1024**3)
-        total_memory_gb = memory_info[1] / (1024**3)
-        used_memory_gb = total_memory_gb - free_memory_gb
-        print(f"{'':>3}  {'Used Memory (GB):':<30}  {used_memory_gb:16.2f}")
-        print(f"{'':>3}  {'Free Memory (GB):':<30}  {free_memory_gb:16.2f}")
-        print(f"{'':>3}  {'Total Memory (GB):':<30}  {total_memory_gb:16.2f}")
+        free_memory_mib = memory_info[0] / (1024**2)
+        total_memory_mib = memory_info[1] / (1024**2)
+        used_memory_mib = total_memory_mib - free_memory_mib
+        print(f"{'':>3}  {'Used Memory (MiB):':<30}  {used_memory_mib:16,.2f}")
+        print(f"{'':>3}  {'Free Memory (MiB):':<30}  {free_memory_mib:16,.2f}")
+        print(f"{'':>3}  {'Total Memory (MiB):':<30}  {total_memory_mib:16,.2f}")
 
         active = "←" if idx == current_idx else ""
         print(f"{'':>3}  {'Active:':<30}  {active:>6}")
